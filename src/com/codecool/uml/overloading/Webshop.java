7794e0e38c08707a291c6a88023890222e6a2c22
Stefan Gherman <scgheman@gmail.com>
2020-04-11 04:52:03 +0300

    Add placeholder classes for other functionalities

--- src/com/codecool/uml/overloading/Webshop.java
+++ src/com/codecool/uml/overloading/Webshop.java
@@ -1,7 +1,28 @@
 package com.codecool.uml.overloading;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Webshop {
 
+    private List<Product> productList = new ArrayList<Product>();
+
+    public void addProduct(Product product) {
+        productList.add(product);
+    }
+
+    public List<Product> getProducts() {
+        return productList;
+    }
+
+    public List<Product> getAllProductsBy(ProductCategory productCategory) {
+        List<Product> productsFilteredByProductByCategory = new ArrayList<Product>();
+        return productsFilteredByProductByCategory;
+    }
+    public List<Product> getAllProductsBy(Supplier supplier) {
+        List<Product> productsFilteredByProductBySupplier = new ArrayList<Product>();
+        return productsFilteredByProductBySupplier;
+    }
     public static void main(String[] args) {
 	// write your code here
     }

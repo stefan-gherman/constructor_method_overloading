f5eecd83d62db5c73c46de5e47d261471b7e0425
Stefan Gherman <scgheman@gmail.com>
2020-04-11 13:09:38 +0300

    Add product class

--- src/com/codecool/uml/overloading/Product.java
+++ src/com/codecool/uml/overloading/Product.java
@@ -1,4 +1,90 @@
 package com.codecool.uml.overloading;
 
+
+import java.util.Currency;
+
 public class Product {
+
+    private static int idIncrementer = 0;
+    private int id = 0;
+    private String name;
+    private float defaultPrice = 0.99f;
+    private Currency defaultCurrency = Currency.getInstance("USD");
+    private ProductCategory productCategory;
+    private Supplier supplier;
+
+
+    public Product() {
+        this.id = idIncrementer;
+        this.name = String.format("Product #%s", this.id);
+        idIncrementer += 1;
+        this.productCategory = null;
+        this.supplier = null;
+    }
+
+    public Product(String name, float defaultPrice, Currency defaultCurrency) {
+        this.id = idIncrementer;
+        idIncrementer+=1;
+        this.name = name;
+        this.defaultPrice = defaultPrice;
+        this.defaultCurrency = defaultCurrency;
+    }
+
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getDefaultPrice() {
+        return this.defaultPrice;
+    }
+
+    public void setDefaultPrice(float defaultPrice) {
+        this.defaultPrice = defaultPrice;
+    }
+
+    public Currency getDefaultCurrency() {
+        return defaultCurrency;
+    }
+
+    public void setDefaultCurrency(Currency defaultCurrency) {
+        this.defaultCurrency = defaultCurrency;
+    }
+
+    public ProductCategory getProductCategory() {
+        return this.productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public Supplier getSupplier() {
+        return this.supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    @Override
+    public String toString() {
+        return "Product:{" +
+                "id=" + this.id +
+                ", name='" + name + '\'' +
+                ", defaultPrice=" + defaultPrice +
+                ", defaultCurrency=" + defaultCurrency +
+                ", productCategory=" + productCategory +
+                ", supplier=" + supplier +
+                '}';
+    }
 }
--- src/com/codecool/uml/overloading/Webshop.java
+++ src/com/codecool/uml/overloading/Webshop.java
@@ -1,6 +1,7 @@
 package com.codecool.uml.overloading;
 
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 
 public class Webshop {
@@ -25,5 +26,17 @@ public class Webshop {
     }
     public static void main(String[] args) {
 	// write your code here
+        Product p1 = new Product();
+        Product p2 = new Product();
+        Product p3 = new Product();
+        Product p4 = new Product();
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+        Currency currency;
+        Product p5 = new Product("Gatorade", 3.99f, currency =  Currency.getInstance("EUR"));
+        System.out.println(p5.toString());
+
     }
 }
